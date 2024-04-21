3edcb490aadadaacab89fc4cb213b3c0c57b29ea
Sowparna Shome <soupornoshome@gmail.com>
2024-04-21 20:56:59 +0530

    order status updated

--- Retail_MVC/Areas/Admin/Controllers/OrderController.cs
+++ Retail_MVC/Areas/Admin/Controllers/OrderController.cs
@@ -4,7 +4,9 @@ using Retail_MVC.DataAccess.Repository.IRepository;
 using Retail_MVC.Models;
 using Retail_MVC.Models.ViewModels;
 using Retail_MVC.Utility;
-using System.Security.Claims;
+using Stripe;
+using Stripe.Checkout;
+
 
 namespace Retail_MVC.Areas.Admin.Controllers
 {
@@ -65,7 +67,130 @@ namespace Retail_MVC.Areas.Admin.Controllers
 
             return RedirectToAction(nameof(Details), new { orderId = orderHeaderFromDb.Id });
         }
-		
 
-	}
+        [HttpPost]
+        [Authorize(Roles =SD.Role_Admin+","+SD.Role_Courier)]
+        public async Task<IActionResult> StartProcessing()
+        {
+            await _unitOfWork.OrderHeader.UpdateStatusAsync(OrderVM.OrderHeader.Id, SD.StatusInProcess);
+            await _unitOfWork.SaveAsync();
+            TempData["Success"] = "Order Details Updated Successfully";
+            return RedirectToAction(nameof(Details), new { orderId = OrderVM.OrderHeader.Id });
+        }
+
+        [HttpPost]
+        [Authorize(Roles = SD.Role_Admin + "," + SD.Role_Courier)]
+        public async Task<IActionResult> ShipOrder()
+        {
+            var orderHeader=await _unitOfWork.OrderHeader.GetAsync(u=>u.Id==OrderVM.OrderHeader.Id);
+            if (orderHeader != null)
+            {
+                orderHeader.TrackingNumber=OrderVM.OrderHeader.TrackingNumber;
+                orderHeader.Carrier=OrderVM.OrderHeader.Carrier;
+                orderHeader.OrderStatus = SD.StatusShipped;
+                orderHeader.ShippingDate=DateTime.Now;
+                //if (orderHeader.PaymentStatus = SD.PaymentStatusDelayedPayment)
+                //{
+                //    orderHeader.PaymentDueDate=DateTime.Now.AddDays(3);
+                //}
+                await _unitOfWork.OrderHeader.UpdateAsync(orderHeader);
+                await _unitOfWork.SaveAsync();
+                TempData["Success"] = "Order Shipped Successfully";
+                return RedirectToAction(nameof(Details), new { orderId = OrderVM.OrderHeader.Id });
+            }
+            return View();
+        }
+
+        [HttpPost]
+        [Authorize(Roles = SD.Role_Admin + "," + SD.Role_Courier)]
+        public async Task<IActionResult> CancelOrder()
+        {
+            var orderHeader=await _unitOfWork.OrderHeader.GetAsync(u=>u.Id ==OrderVM.OrderHeader.Id);   
+            if (orderHeader != null)
+            {
+                if (orderHeader.PaymentStatus == SD.PaymentStatusApproved)
+                {
+                    var options = new RefundCreateOptions
+                    {
+                        Reason = RefundReasons.RequestedByCustomer,
+                        PaymentIntent = orderHeader.PaymentIntentId,
+                    };
+                    var service=new RefundService();
+                    Refund refund=await service.CreateAsync(options);
+                    await _unitOfWork.OrderHeader.UpdateStatusAsync(orderHeader.Id, SD.StatusCancelled, SD.StatusRefunded);
+                }
+                else
+                {
+                    await _unitOfWork.OrderHeader.UpdateStatusAsync(orderHeader.Id,SD.StatusCancelled,SD.StatusCancelled);
+                }
+                await _unitOfWork.SaveAsync();
+                TempData["Success"] = "Order Cancelled Successfully";
+                return RedirectToAction(nameof(Details), new {orderId= orderHeader.Id});    
+            }
+            else
+            {
+                return View();
+            }
+        }
+
+        [ActionName("Details")]
+        [HttpPost]
+        public async Task<IActionResult> Details_PAY_NOW()
+        {
+            OrderVM.OrderHeader=await _unitOfWork.OrderHeader.GetAsync(u=>u.Id==OrderVM.OrderHeader.Id,includeProperties:"ApplicationUser");
+            OrderVM.OrderDetail = await _unitOfWork.OrderDetail.GetAllAsync(u => u.OrderHeaderId == OrderVM.OrderHeader.Id, includeProperties: "Product");
+
+            var domain = Request.Scheme + "://" + Request.Host.Value + "/";
+            var options = new SessionCreateOptions {
+                SuccessUrl = domain + $"admin/order/PaymentConfirmation?orderHeaderId={OrderVM.OrderHeader.Id}",
+                CancelUrl = domain + $"admin/order/details?orderHeaderId={OrderVM.OrderHeader.Id}",
+                LineItems = new List<SessionLineItemOptions>(),
+                Mode="payment"
+            };
+            foreach (var item in OrderVM.OrderDetail)
+            {
+                var sessionLineItem = new SessionLineItemOptions
+                {
+                    PriceData = new SessionLineItemPriceDataOptions
+                    {
+                        UnitAmount = (long)(item.Price * 100), // $20.50 => 2050
+                        Currency = "usd",
+                        ProductData = new SessionLineItemPriceDataProductDataOptions
+                        {
+                            Name = item.Product.Name,
+                        }
+                    },
+                    Quantity = item.Count
+                };
+                options.LineItems.Add(sessionLineItem);
+            }
+            var service = new SessionService();
+            Session session = service.Create(options);
+            await _unitOfWork.OrderHeader.UpdateStripePaymentIdAsync(OrderVM.OrderHeader.Id, session.Id, session.PaymentIntentId);
+            await _unitOfWork.SaveAsync();
+            Response.Headers.Add("Location", session.Url);
+            return new StatusCodeResult(303);
+        }
+
+
+        public async Task<IActionResult> PaymentConfirmation(int orderHeaderId)
+        {
+
+            OrderHeader orderHeader = await _unitOfWork.OrderHeader.GetAsync(u => u.Id == orderHeaderId);
+            if (orderHeader.PaymentStatus == SD.PaymentStatusDelayedPayment)
+            {
+               //delayed payment
+                var service = new SessionService();
+                Session session = service.Get(orderHeader.SessionId);
+
+                if (session.PaymentStatus.ToLower() == "paid")
+                {
+                    await _unitOfWork.OrderHeader.UpdateStripePaymentIdAsync(orderHeaderId, session.Id, session.PaymentIntentId);
+                    await _unitOfWork.OrderHeader.UpdateStatusAsync(orderHeaderId, orderHeader.OrderStatus, SD.PaymentStatusApproved);
+                    await _unitOfWork.SaveAsync();
+                }
+            }
+            return View(orderHeaderId);
+        }
+    }
 }
