47ae0ebdd1bbec2951e4ce7d4ad42339abdbe47f
SowparnaShome <shomeshome234@gmail.com>
2024-04-23 10:40:22 +0530

    removed seeded values of vendor courier and products

--- Retail_MVC.DataAccess/Data/ApplicationDbContext.cs
+++ Retail_MVC.DataAccess/Data/ApplicationDbContext.cs
@@ -25,22 +25,7 @@ namespace Retail_MVC.DataAccess.Data
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
             base.OnModelCreating(modelBuilder);
-            //modelBuilder.Entity<Category>().HasData(
-            //    new Category { Id = 1, Name = "Action", DisplayOrder = 1 },
-            //    new Category { Id = 2, Name = "sci-fi", DisplayOrder = 2 },
-            //    new Category { Id = 3, Name = "horror", DisplayOrder = 3 }
-            //    );
-
-            //modelBuilder.Entity<Product>().HasData(
-            //    new Product { Id=1,Name="product1",Description="desc1",Price=700.00,  CategoryId=1,ImageUrl="" },
-            //    new Product { Id = 2, Name = "product2", Description = "desc2", Price = 800.00,CategoryId=1, ImageUrl = "" },
-            //    new Product { Id = 3, Name = "product3", Description = "desc3", Price = 900.00 ,  CategoryId =2, ImageUrl = "" }
-            //    );
-            modelBuilder.Entity<Vendor>().HasData(
-                new Vendor { Id = 1, Name = "Vendor1", StreetAddress="xyz1",City="abc1",State="state1",PostalCode="876543",PhoneNumber="9876543210"},
-               new Vendor { Id = 2, Name = "Vendor2", StreetAddress = "xyz2", City = "abc2", State = "state2", PostalCode = "876542", PhoneNumber = "9876543211" },
-               new Vendor { Id = 3, Name = "Vendor3", StreetAddress = "xyz3", City = "abc3", State = "state3", PostalCode = "876541", PhoneNumber = "9876543212" }
-                );
+            
         }
     }
 }
